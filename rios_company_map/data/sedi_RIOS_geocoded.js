c59ef036cc725e883a633ec4e054c3f2495493f6
Francesco Bartoli <francesco.bartoli@geobeyond.it>
2012-11-28 12:01:43 +0100

    mappa completa delle sedi rios

---
+++ rios_company_map/data/sedi_RIOS_geocoded.js
@@ -0,0 +1,41 @@
+var riosFeature = {
+        "type": "FeatureCollection",
+                                                                                        
+        "features": [
+        { "type": "Feature", "id": 1, "properties": { "Azienda": "GEOBEYOND S.R.L.", "Indirizzo_Sede": "Via delle Cave 91B, Roma", "CAP": 181, "Sito_Web": "http://www.geobeyond.it", "Latitudine": 41.871775, "Longitudine": 12.530679 }, "geometry": { "type": "Point", "coordinates": [ 12.530679, 41.871775 ] } }
+        ,
+        { "type": "Feature", "id": 2, "properties": { "Azienda": "BINARIO ETICO SOCIETA' COOPERATIVA A.R.L.", "Indirizzo_Sede": "Via del Forte Tiburtino 98/100, Roma", "CAP": 159, "Sito_Web": "http://www.binarioetico.org/", "Latitudine": 41.911413, "Longitudine": 12.556532 }, "geometry": { "type": "Point", "coordinates": [ 12.556532, 41.911413 ] } }
+        ,
+        { "type": "Feature", "id": 3, "properties": { "Azienda": "BINARIO ETICO SOCIETA' COOPERATIVA A.R.L.", "Indirizzo_Sede": "Via dei Sabelli 215, Roma", "CAP": 185, "Sito_Web": "http://www.binarioetico.org/", "Latitudine": 41.898501, "Longitudine": 12.520103 }, "geometry": { "type": "Point", "coordinates": [ 12.520103, 41.898501 ] } }
+        ,
+        { "type": "Feature", "id": 4, "properties": { "Azienda": "ELABOR S.C. ", "Indirizzo_Sede": "Via Garibaldi 33, Pisa", "CAP": 56124, "Sito_Web": "http://www.elabor.biz/", "Latitudine": 43.715039, "Longitudine": 10.410275 }, "geometry": { "type": "Point", "coordinates": [ 10.410275, 43.715039 ] } }
+        ,
+        { "type": "Feature", "id": 5, "properties": { "Azienda": "LYNX S.R.L. ", "Indirizzo_Sede": "Via dei Quintili 25, Roma", "CAP": 175, "Sito_Web": "http://w2.lynxlab.com/", "Latitudine": 41.865126, "Longitudine": 12.546885 }, "geometry": { "type": "Point", "coordinates": [ 12.546885, 41.865126 ] } }
+        ,
+        { "type": "Feature", "id": 6, "properties": { "Azienda": "NOIS3LAB S.N.C. ", "Indirizzo_Sede": "Via Mario Cingolani 18, Roma", "CAP": 155, "Sito_Web": "http://nois3lab.it/", "Latitudine": 41.9141, "Longitudine": 12.581283 }, "geometry": { "type": "Point", "coordinates": [ 12.581283, 41.9141 ] } }
+        ,
+        { "type": "Feature", "id": 7, "properties": { "Azienda": "PRO-NETICS S.P.A. ", "Indirizzo_Sede": "Via Luigi Schiavonetti 270/D, Roma", "CAP": 173, "Sito_Web": "http://www.pro-netics.com/", "Latitudine": 41.845654, "Longitudine": 12.616422 }, "geometry": { "type": "Point", "coordinates": [ 12.616422, 41.845654 ] } }
+        ,
+        { "type": "Feature", "id": 8, "properties": { "Azienda": "SMC TREVISO S.R.L. ", "Indirizzo_Sede": "Via Roma 4, Lancenigo di Villorba,  Treviso", "CAP": 31020, "Sito_Web": "http://www.smc.it/", "Latitudine": 45.66631, "Longitudine": 12.24204 }, "geometry": { "type": "Point", "coordinates": [ 12.24204, 45.66631 ] } }
+        ,
+        { "type": "Feature", "id": 9, "properties": { "Azienda": "SMC TREVISO S.R.L. ", "Indirizzo_Sede": "Via Palladio, 92 Tavagnacco, UDINE", "CAP": 33100, "Sito_Web": "http://www.smc.it/", "Latitudine": 46.114329, "Longitudine": 13.224991 }, "geometry": { "type": "Point", "coordinates": [ 13.224991, 46.114329 ] } }
+        ,
+        { "type": "Feature", "id": 10, "properties": { "Azienda": "SMC TREVISO S.R.L. ", "Indirizzo_Sede": "Via C. Marx 131/2, Carpi, MODENA", "CAP": 41012, "Sito_Web": "http://www.smc.it/", "Latitudine": 44.78131, "Longitudine": 10.8874 }, "geometry": { "type": "Point", "coordinates": [ 10.8874, 44.78131 ] } }
+        ,
+        { "type": "Feature", "id": 11, "properties": { "Azienda": "SMC PADOVA S.R.L.", "Indirizzo_Sede": "Via San Marco 133,  Padova", "CAP": 35120, "Sito_Web": "http://www.smc.it/", "Latitudine": 45.424724, "Longitudine": 11.944939 }, "geometry": { "type": "Point", "coordinates": [ 11.944939, 45.424724 ] } }
+        ,
+        { "type": "Feature", "id": 12, "properties": { "Azienda": "SMC VENEZIA S.R.L.", "Indirizzo_Sede": "Via Garda 7/1, San Donà di Piave, VENEZIA", "CAP": 30027, "Sito_Web": "http://www.smc.it/", "Latitudine": 45.630335, "Longitudine": 12.566955 }, "geometry": { "type": "Point", "coordinates": [ 12.566955, 45.630335 ] } }
+        ,
+        { "type": "Feature", "id": 13, "properties": { "Azienda": "SMC MILANO S.R.L.", "Indirizzo_Sede": "Via Risorgimento 84, Mazzo di Rho, MILANO", "CAP": 20017, "Sito_Web": "http://www.smc.it/", "Latitudine": 45.468945, "Longitudine": 9.18103 }, "geometry": { "type": "Point", "coordinates": [ 9.18103, 45.468945 ] } }
+        ,
+        { "type": "Feature", "id": 14, "properties": { "Azienda": "VIVIEB S.R.L.", "Indirizzo_Sede": "Via Cannuccia 1, Pineto, TERAMO", "CAP": 64025, "Sito_Web": "http://www.vivieb.it", "Latitudine": 42.611649, "Longitudine": 14.062851 }, "geometry": { "type": "Point", "coordinates": [ 14.062851, 42.611649 ] } }
+        ,
+        { "type": "Feature", "id": 15, "properties": { "Azienda": "D'Vel S.n.c.", "Indirizzo_Sede": "Via delle officine 8, Rimini", "CAP": 47900, "Sito_Web": "http://www.d-vel.com", "Latitudine": 44.057279, "Longitudine": 12.581911 }, "geometry": { "type": "Point", "coordinates": [ 12.581911, 44.057279 ] } }
+        ,
+        { "type": "Feature", "id": 16, "properties": { "Azienda": "Thread Solutions s.r.l.", "Indirizzo_Sede": "Via Perosi 4, Rovereto, Trento", "CAP": 38068, "Sito_Web": "http://www.threadsolutions.com", "Latitudine": 45.904128, "Longitudine": 11.043022 }, "geometry": { "type": "Point", "coordinates": [ 11.043022, 45.904128 ] } }
+        ,
+        { "type": "Feature", "id": 17, "properties": { "Azienda": "KELYAN S.r.l. ", "Indirizzo_Sede": "Via Legnano 45, TORINO", "CAP": 10129, "Sito_Web": "http://www.kelyan.com/", "Latitudine": 45.061784, "Longitudine": 7.665504 }, "geometry": { "type": "Point", "coordinates": [ 7.665504, 45.061784 ] } }
+        ,
+        { "type": "Feature", "id": 18, "properties": { "Azienda": "INTEGRAZIONE E SISTEMI S.P.A.", "Indirizzo_Sede": "Via dei Granai di Nerva 48, ROMA", "CAP": 142, "Sito_Web": "http://www.isspa.it/", "Latitudine": 41.834615, "Longitudine": 12.510596 }, "geometry": { "type": "Point", "coordinates": [ 12.510596, 41.834615 ] } }
+
+]};
